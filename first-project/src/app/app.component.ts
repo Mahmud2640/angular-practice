fccfad7970bb2b37d9662069f5940983abf90a34
Mahmud2640 <mdmahmud484@gmail.com>
2023-08-23 09:14:10 +0600

    blog-components

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-project';
+  title = 'Blog Applocation';
+  blogs = [
+    'Blog 1',
+    'Blog 2',
+    'Blog 3',
+    'Blog 4',
+    'Blog 5',
+    'Blog 6',
+    'Blog 7',
+    'Blog 8',
+    'Blog 9',
+    'Blog 10',
+  ];
+
+  getTitle() {
+    return this.title;
+  }
 }
