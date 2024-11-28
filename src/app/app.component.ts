cfe9f014032be51126932826b8954655d3fcf4f4
Mahmud <55327257+Mahmud2640@users.noreply.github.com>
2024-11-28 15:38:47 +0600

    Add AOS library for scroll animations and update Angular configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Blog Applocation';
   blogs = [
     'Blog 1',
@@ -19,6 +20,14 @@ export class AppComponent {
     'Blog 9',
     'Blog 10',
   ];
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1200, // Animation duration
+      once: true, // Whether animation should happen only once
+      mirror: false, // Whether elements should animate out while scrolling past them
+    });
+  }
 
   getTitle() {
     return this.title;
